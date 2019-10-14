cf6ada63dbcd8e8f7304a2bb841c2536e30b4143
girguy <girineza.guy@gmail.com>
2019-10-14 20:31:14 +0200

    Second commit on outliers

--- script_outliers.R
+++ script_outliers.R
@@ -6,19 +6,41 @@ data <- read.table("hotel-maids.txt", sep="\t", header=TRUE, na.strings = "NA")
 attach(data)
 
 # Boxplot of attributes that contains no significant outliers
-boxplot(Wt)
-boxplot(Wt2)
-boxplot(BMI)
-boxplot(BMI2)
-boxplot(Fat)
-boxplot(Fat2)
-boxplot(WHR) 
-boxplot(WHR2) 
+png(file = "boxPlotWt.png")
+bplot <- boxplot(Wt, Wt2, main="Pourcentage of body weigth")
+abline(h=2, col='red')
+dev.off()
 
+png(file = "boxPlotBmi.png")
+boxplot(BMI, BMI2)
+bplot <- boxplot(BMI, BMI2, main="Body mass index")
+abline(h=2, col='red')
+dev.off()
+
+png(file = "boxPlotFat.png")
+boxplot(Fat, Fat2)
+bplot <- boxplot(Fat, Fat2, main="Pourcentage of body fat")
+abline(h=2, col='red')
+dev.off()
+
+png(file = "boxPlotWhr.png")
+boxplot(WHR, WHR2)
+bplot <- boxplot(WHR, WHR2, main="Waist-to-hip ratio")
+abline(h=2, col='red')
+dev.off()
+
+png(file = "boxPlotSyst.png")
+boxplot(Syst, Syst2)
+bplot <- boxplot(Syst, Syst2, main="Systolic blood pressure")
+abline(h=2, col='red')
+dev.off()
+
+boxplot(Diast, Diast2)
+bplot <- boxplot(Diast, Diast2, main="Diastolic blood pressure")
+abline(h=2, col='red')
+dev.off()
 
 # Syst attribute for oultlier analysis
-# Boxplot
-boxplot(Syst)
 # z-Score
 zSyst <- scale(Syst)
 # Graph of the z-Score
@@ -31,8 +53,6 @@ hist(Syst)
 
 
 # Syst2 attribute for oultlier analysis
-# Boxplot
-boxplot(Syst2) # OUTLIERS
 # z-Score
 zSyst2 <- scale(Syst2)
 # Graph of the z-Score
@@ -45,8 +65,6 @@ hist(Syst2)
 
 
 # Diast attribute for oultlier analysis
-# Boxplot
-boxplot(Diast) # OUTLIERS
 # z-Score
 zDiast <- scale(Diast)
 # Graph of the z-Score
