cb4a1e227e93e519a16313b60cc15f169106de22
girguy <girineza.guy@gmail.com>
2019-11-07 12:35:16 +0100

    Cleaning of the data

---
+++ script_cleaning_data.R
@@ -0,0 +1,80 @@
+library(ggplot2)
+library(corrplot)
+"
+This function fill NAs values of vector columns with the mean of this
+column
+
+Parameters
+- columns -> It is a vector containing the column where the
+                          replacement has to be done
+- X -> Dataset
+"
+function_mean<- function(columns, X) {
+  for(i in columns){
+    X[is.na(X[,i]), i] <- mean(X[,i], na.rm = TRUE)
+  }
+  return (X)
+}
+
+"
+This function fills NAs values with their linear regression of the 
+form y = mx + b
+
+Parameters
+- X -> Dataset
+- col1 -> corresponds to the y
+- col2 -> corresponds to the x
+"
+function_linear_regression <- function(X, col1, col2){
+  trainSet <- subset(X, select=c(col1, col2))
+  trainSet <- na.omit(trainSet)
+  # Linear model 
+  linmodel <- lm(trainSet[,1]~trainSet[,2], data=trainSet)
+  # Replacing NAs in the dataset by the regression values
+  X[[col1]][which(is.na(X[col1]))] <- linmodel$coefficients[2]*X[[col2]][which(is.na(X[col1]))]
+  + linmodel$coefficients[1]
+  
+  return (X)
+}
+
+
+data <- read.table("hotel-maids.txt", sep=";", header=TRUE, na.strings = "NA")
+attach(data)
+
+"
+Retrieve all quantitative variables and
+columns with linear relationships with other colums in
+order to avoid singular covariance matrices
+"
+dataC <- subset(data, select = -c(Informed, Fat.Ft2, BMI.BM2, Dst.Ds2))
+# Deletion of multiple values
+# Those deletions will be expalined in the report
+X <- dataC[-c(13,14,16,28,29,30,36,55,60),] #66 obs (88%)
+colMeans(is.na(X))
+"
+We can observe that columns 'WHR', 'Syst', 'Syst2' and 'Diast'
+Only miss one value respectively. Let's replace them by the mean
+In function of the qualitative variable : 'informed'.
+"
+X_0 <- data.frame(X[c(1:34),])
+X_1 <- data.frame(X[-c(1:34),])
+# There is no missing values inside columns of interest in dataset X_1.
+X_0 <- function_mean(c(8, 10, 11, 12), X_0)
+
+"
+Missing values of columns 'Fat', 'Fat2' and 'WHR2'
+will be fill thanks to linear regression in function of the
+qualitative variable : 'informed' and the period when the 
+measure were taken.
+"
+
+X_0 <- function_linear_regression(X_0, "WHR2", "WHR")
+X_0 <- function_linear_regression(X_0, "Fat", "Wt")
+X_0 <- function_linear_regression(X_0, "Fat2", "Wt2")
+
+X_1 <- function_linear_regression(X_1, "WHR2", "WHR")
+X_1 <- function_linear_regression(X_1, "Fat", "Wt")
+X_1 <- function_linear_regression(X_1, "Fat2", "Wt2")
+#Merging of the two dataset X_0 and X_1
+X <- rbind(X_0, X_1)
+colMeans(is.na(X)) # No missing value.
